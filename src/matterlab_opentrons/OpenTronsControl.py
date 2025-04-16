7a7d818474fad2e166079bf0e8ddc007d6eb8352
clascclon <clascclon@gmail.com>
2025-04-15 20:57:03 -0400

    more suitable for SDL2

--- src/matterlab_opentrons/OpenTronsControl.py
+++ src/matterlab_opentrons/OpenTronsControl.py
@@ -1,5 +1,5 @@
 from prefect import flow,task,serve
-from sshclient import SSHClient
+from .sshclient import SSHClient
 # from dotenv import load_dotenv
 import os
 import time 
@@ -262,46 +262,115 @@ class OpenTrons:
 def demo_ot2(simulation:bool = True):
     ot = OpenTrons(host_alias="ot2", password = "accelerate", simulation=simulation)
     ot.home()
-    with open(Path(r"C:\Users\han\Downloads\matter_24_vialplate_3700ul.json"), "r") as f:
+    with open(Path(r"C:\Users\aag\Downloads\matterlab_24_vialplate_3700ul.json"), "r") as f:
         plate_1 = json.load(f)
+    with open(Path(r"C:\Users\aag\Downloads\matterlab_1_beaker_30000ul.json"), "r") as f:
+        beaker_1 = json.load(f)
+    with open(Path(r"C:\Users\aag\Downloads\matterlab_96_tiprack_10ul.json"), "r") as f:
+        tips_1 = json.load(f)
 
     plates = [
-        {"nickname": "vial_24_well", "config": plate_1, "location": "1", "ot_default": False},
-        {"nickname": "plate_96_1", "loadname": "corning_96_wellplate_360ul_flat", "location": "2", "ot_default": True, "config": {}},
+        {"nickname": "plate_96_1", "loadname": "corning_96_wellplate_360ul_flat", "location": "1", "ot_default": True, "config": {}},
+
+        {"nickname": "beaker", "config": beaker_1, "location": "4", "ot_default": False},
+        {"nickname": "vial_24_well_1", "config": plate_1, "location": "5", "ot_default": False},
+        {"nickname": "vial_24_well_2", "config": plate_1, "location": "6", "ot_default": False},
         # {"nickname": "plate_96_2", "loadname": "corning_96_wellplate_360ul_flat", "location": "3", "ot_default": True, "config": {}},
         ]
     tips = [
-        {"nickname": "tip_1000_96_1", "loadname": "opentrons_96_tiprack_1000ul", "location": "8", "ot_default": True, "config": {}},
+        # {"nickname": "tip_20_96_1", "loadname": "opentrons_96_tiprack_20ul", "location": "7", "ot_default": True, "config": {}},
+        {"nickname": "tip_20_96_1", "config": tips_1, "location": "7", "ot_default": False},
+        {"nickname": "tip_300_96_1", "loadname": "opentrons_96_tiprack_300ul", "location": "8", "ot_default": True, "config": {}},
     ]
     for plate in plates:
         ot.load_labware(plate)
     for tip in tips:
         ot.load_labware(tip)
 
-    ot.load_instrument({"nickname": "p1000", "instrument_name": "p1000_single_gen2", "mount": "right", "ot_default": True})
+    ot.load_instrument({"nickname": "p20", "instrument_name": "p20_single_gen2", "mount": "left", "ot_default": True})
+    ot.load_instrument({"nickname": "p300", "instrument_name": "p300_single_gen2", "mount": "right", "ot_default": True})
     
-    ot.get_location_from_labware(labware_nickname="tip_1000_96_1", position= "A1", top=0)
-    ot.pick_up_tip(pip_name="p1000")
+    ot.get_location_from_labware(labware_nickname="tip_300_96_1", position= "A1", top=0)
+    ot.pick_up_tip(pip_name="p300")
+    sample_num=48
+    for i in range(0, sample_num):
+        target_loc = f"{chr(65+i//12)}{i%12+1}"
+        print(f"add to {target_loc}")
 
-    ot.get_location_from_labware(labware_nickname="vial_24_well", position="A3", top=-20)
-    ot.move_to_pip(pip_name="p1000")
-    ot.aspirate(pip_name="p1000", volume=200)
+        ot.get_location_from_labware(labware_nickname="beaker", position="A1", bottom=5)
+        ot.move_to_pip(pip_name="p300")
+        ot.aspirate(pip_name="p300", volume=150)
 
-    ot.get_location_from_labware(labware_nickname="plate_96_1", position="A1", bottom=1)
-    ot.move_to_pip(pip_name="p1000")
-    ot.dispense(pip_name="p1000", volume=100)
+        ot.get_location_from_labware(labware_nickname="plate_96_1", position=target_loc, top=-1)
+        ot.move_to_pip(pip_name="p300")
+        ot.dispense(pip_name="p300", volume=150)
+        ot.blow_out(pip_name="p300")        
+    ot.return_tip(pip_name="p300")
 
+    for i in range(0, 24):
+        source_loc = f"{chr(65+i//6)}{i%6+1}"
+        target_loc = f"{chr(65+i//12)}{i%12+1}"
+
+        ot.get_location_from_labware(labware_nickname="tip_20_96_1", position=target_loc, top=0)
+        ot.pick_up_tip(pip_name="p20")
+
+        ot.get_location_from_labware(labware_nickname="vial_24_well_1", position=source_loc, bottom=5)
+        ot.move_to_pip("p20")
+        ot.aspirate(pip_name="p20", volume=10)
+        
+        ot.get_location_from_labware(labware_nickname="plate_96_1", position=target_loc, top=-1)
+        ot.move_to_pip("p20")
+        ot.dispense(pip_name="p20", volume=10)
+        ot.blow_out(pip_name="p20")
+
+        ot.drop_tip("p20")
     
-    ot.remove_labware(labware_nickname="plate_96_1")
+    for i in range(0, 24):
+        source_loc = f"{chr(65+i//6)}{i%6+1}"
+        target_loc = f"{chr(67+i//12)}{i%12+1}"
 
-    ot.load_labware({"nickname": "plate_96_2", "loadname": "corning_96_wellplate_360ul_flat", "location": "2", "ot_default": True, "config": {}})
+        ot.get_location_from_labware(labware_nickname="tip_20_96_1", position=target_loc, top=0)
+        ot.pick_up_tip(pip_name="p20")
 
-    ot.get_location_from_labware(labware_nickname="plate_96_2", position="H12", top=-1)
-    ot.move_to_pip(pip_name="p1000")
-    ot.dispense(pip_name="p1000", volume=100)
-    ot.blow_out(pip_name="p1000")
+        ot.get_location_from_labware(labware_nickname="vial_24_well_2", position=source_loc, bottom=5)
+        ot.move_to_pip("p20")
+        ot.aspirate(pip_name="p20", volume=10)
+        
+        ot.get_location_from_labware(labware_nickname="plate_96_1", position=target_loc, bottom=1)
+        ot.move_to_pip("p20")
+        ot.dispense(pip_name="p20", volume=10)
+        ot.blow_out(pip_name="p20")
 
-    ot.return_tip(pip_name="p1000")
+        ot.drop_tip("p20")
+        
+        
+    ot.get_location_from_labware(labware_nickname="tip_300_96_1", position= "A1", top=0)
+    ot.pick_up_tip(pip_name="p300")
+    sample_num=48
+    for i in range(0, sample_num):
+        target_loc = f"{chr(65+i//12)}{i%12+1}"
+        print(f"add to {target_loc}")
+
+        ot.get_location_from_labware(labware_nickname="beaker", position="A1", bottom=5)
+        ot.move_to_pip(pip_name="p300")
+        ot.aspirate(pip_name="p300", volume=150)
+
+        ot.get_location_from_labware(labware_nickname="plate_96_1", position=target_loc, top=-1)
+        ot.move_to_pip(pip_name="p300")
+        ot.dispense(pip_name="p300", volume=150)
+        ot.blow_out(pip_name="p300")        
+    ot.return_tip(pip_name="p300")
+    
+    # ot.remove_labware(labware_nickname="plate_96_1")
+
+    # ot.load_labware({"nickname": "plate_96_2", "loadname": "corning_96_wellplate_360ul_flat", "location": "2", "ot_default": True, "config": {}})
+
+    # ot.get_location_from_labware(labware_nickname="plate_96_2", position="H12", top=-1)
+    # ot.move_to_pip(pip_name="p1000")
+    # ot.dispense(pip_name="p1000", volume=100)
+    # ot.blow_out(pip_name="p1000")
+
+    # ot.return_tip(pip_name="p1000")
     
 
     ot.close_session()
@@ -357,5 +426,5 @@ def demo_flex(simulation: bool = True):
 
 
 if __name__ == "__main__":
-    # demo_ot2(simulation=True)
-    demo_flex(simulation=True)
+    demo_ot2(simulation=False)
+    # demo_flex(simulation=True)
---
+++ src/matterlab_opentrons/__init__.py
@@ -0,0 +1,5 @@
+from .OpenTronsControl import OpenTrons
+from .sshclient import SSHClient
+from .well_plate import WellPlateGenerator
+
+__all__ = ["OpenTrons", "SSHClient", "WellPlateGenerator"]
